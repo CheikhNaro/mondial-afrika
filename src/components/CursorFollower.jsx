982268bc2e3d8215a0b885bd3f94c13f5c543197
CheikhDaouda <yousseph.daouda@waouhmonde.com>
2026-06-14 17:56:58 +0100

    hide follower dot on mobile screen

--- src/components/CursorFollower.jsx
+++ src/components/CursorFollower.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function CursorFollower() {
   useEffect(() => {
-    if (window.matchMedia('(hover: none) and (pointer: coarse)').matches) return;
+    if (window.innerWidth < 768) return;
     let cursor;
     import('cursor-effects').then(({ followingDotCursor }) => {
       cursor = new followingDotCursor({ color: '#7C87F0', zIndex: '100' });
